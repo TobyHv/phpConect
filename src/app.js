1b1a8953c1a7d422b49a865618213205e0c22a80
TobyHv <andresjosue@hotmail.es>
2025-02-01 13:09:52 -0600

    app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
+//Importa el módulo Express que es un framework para manejar servidores HTTP en Node.js.
 const express = require('express');
+//Crea una instancia de la aplicación Express, que se usará para definir rutas y manejar solicitudes.
 const app = express();
-
+//Middleware que permite a Express interpretar JSON en las solicitudes entrantes. Esto es útil para manejar datos en formato JSON en peticiones POST, PUT, PATCH.
 app.use(express.json());
 
+//Define una ruta HTTP tipo GET en la raíz ('/').
+//Cuando se hace una solicitud GET a '/', el servidor responde con el texto "Hola mundo".
 app.get('/', (reg, res) => {
     res.send('Hola mundo');
 });
 
+//Exporta la instancia de app para que pueda ser utilizada en otros archivos
 module.exports = app;
