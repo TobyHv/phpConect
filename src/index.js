1b0aab936a4235aa07825468171e0f14b1451ab6
TobyHv <andresjosue@hotmail.es>
2025-02-01 13:16:00 -0600

    index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+//Importa la aplicación Express desde app.js.
 const app =require('./app');
+//Puerto libre que usara el servidor
 const port = 5500;
-
+//Indica que puede manejar operaciones asíncronas
 (async () =>{
+    //Muestra un mensaje inicial antes de iniciar el servidor
     console.log('Sarting server...');
     app.listen(port, () => {
+        //Muestra el puerto donde se ejecuta el servidor
         console.log(`Server is running on port ${port}`);
 
     });
